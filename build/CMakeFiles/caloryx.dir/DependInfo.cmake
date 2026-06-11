7a1769b905a24277e7bbc885a79d1a041c1befa8
Dima <lysakd049@gmail.com>
2026-06-11 11:58:45 +0300

    Add new function on web

---
+++ build/CMakeFiles/caloryx.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Dima/Desktop/Kursa/src/Admin/admin.cpp" "CMakeFiles/caloryx.dir/src/Admin/admin.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Admin/admin.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Category/category.cpp" "CMakeFiles/caloryx.dir/src/Category/category.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Category/category.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Food/food.cpp" "CMakeFiles/caloryx.dir/src/Food/food.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Food/food.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/FoodsDatabase/FoodsDatabase.cpp" "CMakeFiles/caloryx.dir/src/FoodsDatabase/FoodsDatabase.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/FoodsDatabase/FoodsDatabase.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Meal/meal.cpp" "CMakeFiles/caloryx.dir/src/Meal/meal.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Meal/meal.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Models/CalorieCalculator.cpp" "CMakeFiles/caloryx.dir/src/Models/CalorieCalculator.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Models/CalorieCalculator.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Plan/plan.cpp" "CMakeFiles/caloryx.dir/src/Plan/plan.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Plan/plan.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Report/report.cpp" "CMakeFiles/caloryx.dir/src/Report/report.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Report/report.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Services/AuthService.cpp" "CMakeFiles/caloryx.dir/src/Services/AuthService.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Services/AuthService.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Services/CatalogService.cpp" "CMakeFiles/caloryx.dir/src/Services/CatalogService.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Services/CatalogService.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Settings/settings.cpp" "CMakeFiles/caloryx.dir/src/Settings/settings.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Settings/settings.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/Storage/AccountStorage.cpp" "CMakeFiles/caloryx.dir/src/Storage/AccountStorage.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/Storage/AccountStorage.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/User/user.cpp" "CMakeFiles/caloryx.dir/src/User/user.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/User/user.cpp.obj.d"
+  "C:/Users/Dima/Desktop/Kursa/src/main.cpp" "CMakeFiles/caloryx.dir/src/main.cpp.obj" "gcc" "CMakeFiles/caloryx.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
